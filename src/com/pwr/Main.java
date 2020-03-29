465951188a327219ddedde0c06cde3b53d838330
Marcel Guzik <mguzik555@gmail.com>
2020-03-29 01:42:03 +0100

    Initial commit

---
+++ src/com/pwr/Main.java
@@ -0,0 +1,10 @@
+package com.pwr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2;
+        String b = a.toString();
+        System.out.println(b);
+    }
+}
