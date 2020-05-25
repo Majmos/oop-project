c85cb02e69c1ebac612980cdb65364268b947da6
Majmos <miloszskora123@wp.pl>
2020-05-25 22:54:15 +0200

    Update AiBehaviour.java

--- src/main/java/pwr/sim/animal/ai/AiBehaviour.java
+++ src/main/java/pwr/sim/animal/ai/AiBehaviour.java
@@ -6,7 +6,19 @@ import java.util.List;
 import java.util.Stack;
 
 public abstract class AiBehaviour {
-    public abstract void update();
+    public void update() {
+        if(phase == 0) {
+            x += 1;
+        } else if(phase == 1) {
+            y += 1;
+        } else if(phase == 2) {
+            x -= 1;
+        } else if(phase == 3) {
+            y -= 1;
+        }
+        phase = (phase + 1)%4;
+    }
 
     protected IAiState[] currentState;
+    private int phase = 0;
 }
