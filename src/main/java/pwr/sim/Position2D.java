6d8104ff523397fcceecad999b43d7ea2e301e74
Marcel Guzik <mguzik555@gmail.com>
2020-06-08 23:15:06 +0200

    Added documentation for Position2D, Tile, Vector2D, Renderer

--- src/main/java/pwr/sim/Position2D.java
+++ src/main/java/pwr/sim/Position2D.java
@@ -2,19 +2,37 @@ package pwr.sim;
 
 import java.util.Objects;
 
-public class Position2D implements Cloneable {
+/**
+ * Reprezentuje pozycję w świecie. Gwarantuje że pozycja zawsze jest w granicach świata gry.
+ */
+public class Position2D {
+    /**
+     * Tworzy nową pozycję w świecie.
+     * @param x
+     * @param y
+     * @param world
+     */
     public Position2D(int x, int y, World world) {
         this.x = x;
         this.y = y;
         this.world = world;
     }
 
+    /**
+     * Kopiuje pozycję.
+     * @param other
+     */
     public Position2D(Position2D other) {
         this.x = other.x;
         this.y = other.y;
         this.world = other.world;
     }
 
+    /**
+     * Zwraca nową pozycję zmienioną o offset zdefiniowany przez parametry.
+     * @param x przesunięcie pozycji w osi x
+     * @param y przesunięcie pozycji w osi y
+     */
     public void move(int x, int y) {
         int newx = this.x + x;
         int newy = this.y + y;
@@ -25,17 +43,34 @@ public class Position2D implements Cloneable {
         }
     }
 
+    /**
+     * Zmienia pozycję w obrębie tego samego świata.
+     * @param x nowa pozycja w osi x
+     * @param y nowa pozycja w osi y
+     */
     public void setPosition(int x, int y) {
+        if(world.getTile(x, y) == null) {
+            return;
+        }
         this.x = x;
         this.y = y;
     }
 
-    // probably dying in allocations but muh OOP
+    /**
+     * Zwraca kwadrat dystansu do pozycji przekazanej w argumencie.
+     * @param other pozycja do której chcemy uzyskać dystans
+     * @return kwadrat dystansu do pozycji
+     */
     public int distanceSquared(Position2D other) {
         Vector2D delta = delta(other);
         return delta.x * delta.x + delta.y * delta.y;
     }
 
+    /**
+     * Zwraca wektor różnicy między pozycjami. Np. delta między pozycjami {1, 2}, {3, 1} to (2, -1)
+     * @param other pozycja do której różnicę chcemy uzyskać
+     * @return wektor różnicy do określonej pozycji
+     */
     public Vector2D delta(Position2D other) {
         int deltaX = other.getX() - x;
         int deltaY = other.getY() - y;
--- src/main/java/pwr/sim/renderer/Renderer.java
+++ src/main/java/pwr/sim/renderer/Renderer.java
@@ -4,9 +4,21 @@ import pwr.sim.Position2D;
 import pwr.sim.World;
 import pwr.sim.animal.Animal;
 
+/**
+ * Klasa zajmująca się rysowaniem symulacji na ekranie terminala. Posiada metody do poruszania kursorem i wypisywania
+ * tekstu w różnych kolorach.
+ */
 public class Renderer {
+    /**
+     * Ilość znaków jaki zajmuje komórka w kierunku poziomym. W zależności od używanego fontu należy dopasować by
+     * komórka miała możliwie najbliżej kształt kwadratu.
+     */
     static final int cellWidth = 3;
 
+    /**
+     * Rysuje "UI" które składa się z listy aktualnie żyjących zwierząt i ich statusów.
+     * @param world świat dla którego chcemy narysować UI
+     */
     public static void drawUi(World world) {
         // draw animal list UI
         Renderer.setCursorPosition(52 * cellWidth, 1);
@@ -22,30 +34,64 @@ public class Renderer {
         }
     }
 
+    /**
+     * Ustawia pozycję kursora w terminalu na kolumnę `x` i wiersz `y`.
+     * Komórki i wiersze terminala adresowane są od 1.
+     *
+     * Jeżeli `x` > szerokości terminala lub `y` > wysokości terminala, kursor ustawiany jest odpowiednio na ostatnią
+     * kolumnę/wiersz.
+     * @param x kolumna terminala
+     * @param y wiersz terminala
+     */
     public static void setCursorPosition(int x, int y) {
         System.out.print(String.format("\u001B[%d;%dH", y, x));
     }
 
+    /**
+     * Ustawia kursor terminala na pozycję którą zajmuje wybrana komórka świata.
+     * @param position pozycja w świecie na którą chcemy ustawić kursor.
+     */
     public static void setCursorToCell(Position2D position) {
         setCursorPosition(position.getX() * cellWidth + 2, position.getY() + 1);
     }
 
+    /**
+     * Wypisuje tekst z określonym kolorem czcionki i tła.
+     * @param text tekst który chcemy wypisać
+     * @param fgColour kolor czcionki
+     * @param bgColour kolor tła
+     */
     public static void drawColouredText(String text, int fgColour, int bgColour) {
         System.out.print(String.format("\u001B[38;5;%dm\u001B[48;5;%dm%s\u001B[0m", fgColour, bgColour, text));
     }
 
+    /**
+     * Wypisuje znak z określonym kolorem czcionki i tła.
+     * @param c znak który chcemy wypisać
+     * @param fgColour kolor czcionki
+     * @param bgColour kolor tła
+     */
     public static void drawColouredChar(char c, int fgColour, int bgColour) {
         System.out.print(String.format("\u001B[38;5;%dm\u001B[48;5;%dm%c\u001B[0m", fgColour, bgColour, c));
     }
 
+    /**
+     * Uruchamia alternatywny bufor terminala.
+     */
     public static void enableAlternateScreenBuffer() {
         System.out.print("\u001B[?1049h");
     }
 
+    /**
+     * Wyłącza alternatywny bufor terminala.
+     */
     public static void disableAlternateScreenBuffer() {
         System.out.print("\u001B[?1049l");
     }
 
+    /**
+     * Czyści ekran
+     */
     public static void clearScreen() {
         System.out.print("\u001B[2J");
     }
